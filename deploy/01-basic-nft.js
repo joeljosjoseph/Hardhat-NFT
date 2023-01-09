1649cdfb122ff604fd0bea321f7a6b74a23bd947
joeljosjoseph <joeljosjoseph12@gmail.com>
2023-01-09 21:42:13 +0530

    BasicNft tests added

---
+++ deploy/01-basic-nft.js
@@ -0,0 +1,30 @@
+const { network, ethers } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  console.log("--------------------");
+  const args = [];
+
+  const basicNft = await deploy("BasicNft", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    console.log("Verifying.....");
+    await verify(basicNft.address, args);
+  }
+
+  console.log("----------------------");
+};
+
+module.exports.tags = ["all", "basicnft", "main"];
